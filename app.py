a91738791a1140b2f7c6420664a8c9d432ff2edc
Rajeev Ramesh <rajeevramesh21@gmail.com>
2020-07-04 11:12:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+from werkzeug import secure_filename
 
 app = Flask(__name__)
 model = pickle.load(open('resumemodel.pkl', 'rb'))
@@ -8,25 +9,38 @@ model = pickle.load(open('resumemodel.pkl', 'rb'))
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/getfile', methods=['GET','POST'])
+def getfile():
+    if request.method == 'POST':
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    file1 = open("content.txt","a") 
-    int_features = [str(x) for x in request.form.values()]
-    #final_features = [int_features]
-    prediction = model.predict(int_features)
+        # for secure filenames. Read the documentation.
+        file = request.files['myfile']
+        filename = secure_filename(file.filename) 
 
-    output = prediction
+        # os.path.join is used so that paths work in every operating system
+        file.save(os.path.join("C:","Users","lenovo","Desktop","Resume Shortlister",filename))
 
-    if(output=='Data Science'):
-        output1=' Resume has been shortlisted '
+        # You should use os.path.join here too.
+        with open("C:/Users/lenovo/Desktop/Resume Shortlister") as f:
+            file_content = f.read()
+        features=[file_content]
+        model.predict(features)
+        prediction = model.predict(features)
+
+        output = prediction
+
+        if(output=='Data Science'):
+            output1=' Resume has been shortlisted '
+        else:
+            output1='Resume not selected'
+
+        return render_template('index.html', prediction_text=output1+ 'Resume Category is {}'.format(output))
+
+
     else:
-        output1='Resume not selected'
+        result = request.args.get['myfile']
+    return result
 
-    return render_template('index.html', prediction_text=output1+ 'Resume Category is {}'.format(output))
 
 
 if __name__ == "__main__":
