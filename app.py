0fafb29808e338e8d57cfdc1406e3256da1f39a8
Rajeev Ramesh <rajeevramesh21@gmail.com>
2020-07-04 18:10:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template, redirect
+import pickle
+from werkzeug import secure_filename
+
+app = Flask(__name__)
+model = pickle.load(open('resumemodel.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+	
+@app.route('/predict',methods=['POST'])
+def predict():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      with open ('file.txt') as fo:
+          for rec in fo:
+              data=rec
+      return 'file uploaded successfully'
+if __name__ == "__main__":
+    app.run(debug=True)
